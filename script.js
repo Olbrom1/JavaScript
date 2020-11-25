e3a13b820b697360d196eef6bdb24dc12f605141
Olbrom1 <olbrom16@gmail.com>
2020-11-25 11:47:03 +0100

    Project

---
+++ script.js
@@ -0,0 +1,18 @@
+function kubekKlik(evt) {
+    console.log('Klikniety');
+    let poziom = Number(this.getAttribute('poziom'));
+    poziom += 24;
+    if(poziom <= 96){
+        this.style.backgroundPosition = "0 " + poziom + "%";
+        this.setAttribute('poziom', poziom);
+    } else {
+        this.style.cursor = "not-allowed";
+    }
+    
+    
+}
+
+let kubki = document.querySelectorAll('.kubek');
+kubki.forEach(kubek => {
+    kubek.addEventListener('click', kubekKlik);
+})
